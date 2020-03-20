df004f62885a04f5cefef02e332aa2864c5fdbfe
Phoebe Johnson <phoebejohnson@Phoebes-MacBook-Air.local>
2020-03-20 15:05:00 -0700

    jquery ish

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
- document.getElementById("calc").onclick = function () {
-     calcTip();
+$(document).ready(() => {
+  $("#calc").click(() => {
+    let bill = $("#billAmt").val();
+    calcTip(bill);
+    let percent = $("#percentage").val();
+    calcTip(percent);
+  });
  }
 
  function calcTip() {
-     var bill = document.getElementById("billAmt").value;
-     var percent = document.getElementById("percentage").value;
+     var bill = $("#billAmt").val();
+     var percent = $("#percentage").val():
 
      if (billAmt === "" || serviceQual == 0) {
          alert("Please enter values");
          return;
 
-         var tip = bill * (percent / 100)
+     var tip = bill * (percent / 100)
 
-         if (percent == "30") {
-             var tip = bill * 0.3;
-         } else if (percent == "20") {
-             var tip = bill * 0.2;
-         } else if (percent == "15") {
-             var tip = bill * 0.15;
-         } else if (percent == "10") {
-             var tip = bill * 0.1;
-         } else if (percent == "5") {
-             var tip = bill * 0.05;
-         }
-
-         var total = Math.round(tip);
-         document.getElementById("total").style.disply = "block";
-         document.getElementById("tip").innerHTML = total;
+     var total = Math.round(tip);
+     $("#total").css("disply", "block");
+     $("#tip").innerHTML = total;
      }
+
